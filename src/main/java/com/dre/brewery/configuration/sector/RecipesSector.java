fec7fa0e3a91d0315cb0df9c99e1e92a636d2534
jessicaburnsbrandtcamryn278 <suiianbc@icloud.com>
2025-09-28 19:12:38 +0200

    chore(recipe-defaults): don't load the example recipe on startup
    It contains plugin-items that are most likely not present on startup, which would throw errors and it doesn't make sense to have it enabled anyway, as it's only there to show how recipes are specified

--- src/main/java/com/dre/brewery/configuration/sector/RecipesSector.java
+++ src/main/java/com/dre/brewery/configuration/sector/RecipesSector.java
@@ -33,7 +33,7 @@ public class RecipesSector extends AbstractOkaeriConfigSector<ConfigRecipe> {
     // Comments not supported here
 
     ConfigRecipe ex = ConfigRecipe.builder()
-        .enabled(true)
+        .enabled(false)
         .name("Bad Example/Example/Good Example")
         .ingredients(List.of("Bedrock/2", "Spruce_Planks/8", "Bedrock/1", "Brewery:Wheatbeer/2", "ExoticGarden:Grape/3", "ex-item/4"))
         .cookingTime(3)
